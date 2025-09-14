c7e4eb26236dbd75a3a3bed7a70bcaf6aa026277
LengineerC <lengineerc@outlook.com>
2025-09-14 17:32:06 +0800

    fix: 导出方式

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import NcmConvertor from "./NcmConvertor";
 
+export { NcmConvertor };
 export default NcmConvertor;
+
+if (typeof module === 'object' && module.exports) {
+    module.exports = NcmConvertor;
+    module.exports.default = NcmConvertor;
+    module.exports.NcmConvertor = NcmConvertor;
+}
